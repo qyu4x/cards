76e05e9bedb2e34a19d8f7fa997d8a3c4403ea67
qyu4x <qq.khoiri@gmail.com>
2026-01-16 15:18:49 +0700

    elixir warmup

--- lib/cards.ex
+++ lib/cards.ex
@@ -4,7 +4,30 @@ defmodule Cards do
   end
 
   def create_deck do
-    ["Ace", "Joker", "One", "Two", "Three"]
+    values = ["Ace", "Five", "One", "Two", "Three"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # transform into new list of suits.
+    # for suit <- suits do
+    #   suit
+    # end
+
+    # transform into single list of combination value and suits
+    # bad way
+    # combinations =
+    #   for value <- values do
+    #     for suit <- suits do
+    #       "#{value} of #{suit}"
+    #     end
+    #   end
+
+    # List.flatten(combinations)
+
+    # same result with more good way
+    # left side is second loop, righ side is first loop
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
@@ -13,5 +36,10 @@ defmodule Cards do
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def deal(deck, deck_size) do
+    # return value here is tupple the first idx is deck_size, and secondary idx is remaining deck
+    Enum.split(deck, deck_size)
   end
 end
