f3a8a302c7e585b37ee646757b64467401509469
qyu4x <qq.khoiri@gmail.com>
2026-01-16 11:55:04 +0700

    working with Enum

--- lib/cards.ex
+++ lib/cards.ex
@@ -6,4 +6,12 @@ defmodule Cards do
   def create_deck do
     ["Ace", "Joker", "One", "Two", "Three"]
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
 end
